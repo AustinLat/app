2b686a7169fcd4655b85309ee79c080d24c096ff
Austin <alaturno@gmail.com>
2020-12-12 14:52:15 -0800

    "using kivy"

--- locator.py
+++ locator.py
@@ -1,6 +1,17 @@
-# This will be an app to mark site locations and later perhaps add notes like gate codes and maybe even pictures.
+# This will be an app to mark site locations and later perhaps 
+# add notes like gate codes and maybe even pictures.
 
-class Locator():
+from kivy.app import App
+from kivy.uic.label import Label
+
+class Locator(App):
     
-    def main(self):
-        
+    def location(self):
+        label = Label(text = 'Hello',
+                      size_hint=(.5, .5},
+                      pos_hint={'center_x': .5, 'center_y': .5})
+        return label
+
+if __name__ == '__main__':
+    app = MainApp()
+    app.run()
