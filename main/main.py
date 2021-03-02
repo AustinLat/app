7af619e3bab61cb30d79241d216461df14089ef9
AustinLat <alaturno@gmail.com>
2021-03-01 19:04:44 -0800

    text input capture working.

--- main/main.py
+++ main/main.py
@@ -1,7 +1,5 @@
 
 import kivy
-import random
-
 #from plyer import gps
 from kivy.app import App
 from kivy.uix.button import Button
@@ -9,8 +7,7 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 
-class MyApp(App):
-
+class MyApp(App):    
     def build(self):
         layout = BoxLayout(padding=10, orientation='vertical')
         
@@ -21,10 +18,19 @@ class MyApp(App):
         self.lbl1 = Label(text="Enter site name")
         layout.add_widget(self.lbl1)
         
-        self.txt1 = TextInput(text='', multiline=False)
+        self.txt1 = TextInput(multiline=False)
+        self.txt1.bind(text = self.on_text)
         layout.add_widget(self.txt1)
         
         return layout
+    def on_text(self, instance, value):
+        self.site_name = value
+        
+    def buttonClicked(self, btn): 
+        f = open("site_locations.txt","a+")
+        f.write(f'{self.site_name} :\n')
+        f.close()
+
 
 #    def on_start(self):
 #        gps.configure(on_location = self.on_gps_location)
@@ -34,13 +40,6 @@ class MyApp(App):
 #        lat = kwargs['lat']
 #        lon = kwargs['lon']
 #        gps_location = f'{lat} {lon}'
-
-
-    def buttonClicked(self,btn):
-        site_name = self.txt1.text
-        f = open("site_locations.txt","a+")
-        f.write(f'{site_name} \n')
-        f.close()
 
 # run app
 if __name__ == "__main__":
